a4857edb5bf6f0460a15a8051d74cc1fae768114
JozefK <164421631+jozefkubis@users.noreply.github.com>
2024-08-03 08:25:43 +0200

    Premenovany App na App-useReducer-Context

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import App from "./App"
+import App from "./App-useReducer-Context"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App />)
